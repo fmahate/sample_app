f8bbce1aa816234ddb530a4d14b40ddca38eda38
Fazal Mahate <fmahate@yahoo.com>
2012-04-05 19:52:30 -0400

    Completes user sign in

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@ SampleApp::Application.routes.draw do
 
   resources :users
 
+  resources :sessions, :only => [:new, :create, :destroy]
+
   root :to => 'pages#home'
   
-  match '/about', :to => 'pages#about'
-  match '/contact', :to => 'pages#contact'
-  match '/help', :to => 'pages#help'
-  match '/signup', :to => 'users#new'
+  match '/about',    :to => 'pages#about'
+  match '/contact',  :to => 'pages#contact'
+  match '/help',     :to => 'pages#help'
+  match '/signup',   :to => 'users#new'
+  match '/signin',   :to => 'sessions#new'
+  match '/signout',  :to => 'sessions#destroy'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
